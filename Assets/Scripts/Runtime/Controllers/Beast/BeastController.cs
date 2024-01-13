a5f4bd33ecc1891cc11d9abc1f640304ec5ba088
EmreOzhazer <emreozhazer@hotmail.com>
2024-01-13 18:59:43 +0300

    beast navmesh added

---
+++ Assets/Scripts/Runtime/Controllers/Beast/BeastController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Runtime.Controllers.Beast
+
+{
+    public class BeastController : MonoBehaviour
+    {
+        public NavMeshAgent beast;
+        public Transform player;
+
+        private void Update()
+        {
+            beast.SetDestination(player.position);
+            
+        }
+    }
+}
