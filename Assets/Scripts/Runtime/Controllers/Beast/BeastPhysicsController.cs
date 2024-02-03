688a5d2acb0f61db759e3351907a06c1cbbcd818
EmreOzhazer <emreozhazer@hotmail.com>
2024-02-03 14:13:07 +0300

    Beast jumpscare added

---
+++ Assets/Scripts/Runtime/Controllers/Beast/BeastPhysicsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DG.Tweening;
+using Runtime.Controllers.Beast;
+using Runtime.Controllers.Pool;
+using Runtime.Enums;
+using Runtime.Managers;
+using Runtime.Signals;
+using UnityEngine;
+
+namespace Runtime.Controllers.Beast
+{
+    public class BeastPhysicsController : MonoBehaviour
+    {
+        private readonly string _beastJumpscare = "BeastJumpscare";
+        private readonly string _player = "Player";
+        [SerializeField] private BeastController beastController;
+        [SerializeField] private GameObject jumpscarePrefab;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            
+            if (other.CompareTag(_player))
+            { 
+                beastController.Jumpscare(jumpscarePrefab);
+                Debug.Log("JUMPSCARE");
+            }
+        }
+        
+    }
+}
