73e52e27176c4ca1be958e99d9a06976aed4714c
EmreOzhazer <emreozhazer@hotmail.com>
2024-04-05 16:50:16 +0300

    task tab text changes with animator trigger added

---
+++ Assets/Scripts/Runtime/Controllers/Task Tab/TaskTabController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using Runtime.Enums;
+using TMPro;
+using UnityEngine;
+
+namespace Runtime.Controllers.Task_Tab
+{
+    public class TaskTabController: MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI taskText;
+        [SerializeField] private Animator taskTabAnim;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("tasks"))
+            {
+                taskTabAnim.SetTrigger(TaskAnimationStates.BringTask.ToString());
+                Debug.Log("task debug");
+                taskTabAnim.Play("taskanim", -1, 0f);
+                taskText.text = "Collect flowers 0/10";
+            }
+        }
+
+        private void Start()
+        {
+            Invoke("PickUpPhone",5f);
+        }
+        
+
+        private void PickUpPhone()
+        {
+            taskTabAnim.SetTrigger(TaskAnimationStates.BringTask.ToString());
+            taskText.text = "Pick up the phone";
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("tasks"))
+            {
+                
+                taskTabAnim.SetTrigger(TaskAnimationStates.RemoveTask.ToString());
+            }
+
+        }
+    }
+}
