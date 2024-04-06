331f36a87e607089ddf8a47e3d9287df332016fd
By4r <j.alfarno142@gmail.com>
2024-04-06 19:21:19 +0300

    Day Control

--- Assets/Scripts/Runtime/Managers/DayManager.cs
+++ Assets/Scripts/Runtime/Managers/DayManager.cs
@@ -9,33 +9,40 @@ public class DayManager : MonoBehaviour
     //Variables
     [SerializeField, Range(0, 24)] private float TimeOfDay;
 
+    private bool _cycleActive;
 
     private void Update()
     {
         if (Preset == null)
             return;
 
-        if (Application.isPlaying)
+        if (_cycleActive)
         {
-            //(Replace with a reference to the game time)
-            TimeOfDay += Time.deltaTime;
-            TimeOfDay %= 24; //Modulus to ensure always between 0-24
+            TimeOfDay += Time.deltaTime; 
+            TimeOfDay %= 24; 
             UpdateLighting(TimeOfDay / 24f);
+
+
+            if (TimeOfDay >= 2f)
+            {
+                _cycleActive = false;
+            }
         }
-        else
+
+        if (Input.GetKeyDown(KeyCode.N))
         {
-            UpdateLighting(TimeOfDay / 24f);
+            _cycleActive = !_cycleActive; 
         }
     }
 
 
     private void UpdateLighting(float timePercent)
     {
-        //Set ambient and fog
+        
         RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
         RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
 
-        //If the directional light is set then rotate and set it's color, I actually rarely use the rotation because it casts tall shadows unless you clamp the value
+        
         if (DirectionalLight != null)
         {
             DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
@@ -45,18 +52,18 @@ public class DayManager : MonoBehaviour
 
     }
 
-    //Try to find a directional light to use if we haven't set one
+    
     private void OnValidate()
     {
         if (DirectionalLight != null)
             return;
 
-        //Search for lighting tab sun
+        
         if (RenderSettings.sun != null)
         {
             DirectionalLight = RenderSettings.sun;
         }
-        //Search scene for light that fits criteria (directional)
+        
         else
         {
             Light[] lights = GameObject.FindObjectsOfType<Light>();
