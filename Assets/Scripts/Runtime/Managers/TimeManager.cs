1bf1c74a16c8c3b651966d0f52cfc2261b5bdf68
By4r <j.alfarno142@gmail.com>
2024-05-13 13:25:05 +0300

    Time Manager - Day Cycle

--- Assets/Scripts/Runtime/Managers/TimeManager.cs
+++ Assets/Scripts/Runtime/Managers/TimeManager.cs
@@ -1,48 +1,110 @@
-﻿using System;
-using Runtime.Controllers.PlayTime;
-using Runtime.Signals;
+﻿using System.Collections;
 using UnityEngine;
 
 namespace Runtime.Managers
 {
     public class TimeManager : MonoBehaviour
     {
-        #region TimeManager Variables
-
-        #region Serialized Variables
-
-        [SerializeField] private TimeController timeController;
-
-        [SerializeField] private float _startTimeDelay;
-
-        #endregion
-
-        #endregion
-
-        private void OnEnable()
+        [SerializeField] private Texture2D skyboxNight;
+        [SerializeField] private Texture2D skyboxSunrise;
+        [SerializeField] private Texture2D skyboxDay;
+        [SerializeField] private Texture2D skyboxSunset;
+ 
+        [SerializeField] private Gradient graddientNightToSunrise;
+        [SerializeField] private Gradient graddientSunriseToDay;
+        [SerializeField] private Gradient graddientDayToSunset;
+        [SerializeField] private Gradient graddientSunsetToNight;
+ 
+        [SerializeField] private Light globalLight;
+ 
+        private int minutes;
+ 
+        public int Minutes
+        { get { return minutes; } set { minutes = value; OnMinutesChange(value); } }
+ 
+        private int hours = 5;
+ 
+        public int Hours
+        { get { return hours; } set { hours = value; OnHoursChange(value); } }
+ 
+        private int days;
+ 
+        public int Days
+        { get { return days; } set { days = value; } }
+ 
+        private float tempSecond;
+ 
+        public void Update()
         {
-            SubscribeEvents();
+            tempSecond += Time.deltaTime;
+ 
+            if (tempSecond >= 1)
+            {
+                Minutes += 1;
+                tempSecond = 0;
+            }
         }
-
-        private void SubscribeEvents()
+ 
+        private void OnMinutesChange(int value)
         {
-            TimeSignals.Instance.onTimeStarted += OnTimeStarted;
+            globalLight.transform.Rotate(Vector3.up, (1f / (1440f / 4f)) * 360f, Space.World);
+            if (value >= 60)
+            {
+                Hours++;
+                minutes = 0;
+            }
+            if (Hours >= 24)
+            {
+                Hours = 0;
+                Days++;
+            }
         }
-
-        private void OnTimeStarted()
+ 
+        private void OnHoursChange(int value)
         {
-            //timeController.StartTimer();
-            timeController.StartTimeWithDelay(_startTimeDelay);
+            if (value == 6)
+            {
+                StartCoroutine(LerpSkybox(skyboxNight, skyboxSunrise, 10f));
+                StartCoroutine(LerpLight(graddientNightToSunrise, 10f));
+            }
+            else if (value == 8)
+            {
+                StartCoroutine(LerpSkybox(skyboxSunrise, skyboxDay, 10f));
+                StartCoroutine(LerpLight(graddientSunriseToDay, 10f));
+            }
+            else if (value == 18)
+            {
+                StartCoroutine(LerpSkybox(skyboxDay, skyboxSunset, 10f));
+                StartCoroutine(LerpLight(graddientDayToSunset, 10f));
+            }
+            else if (value == 22)
+            {
+                StartCoroutine(LerpSkybox(skyboxSunset, skyboxNight, 10f));
+                StartCoroutine(LerpLight(graddientSunsetToNight, 10f));
+            }
         }
-
-        private void UnSubscribeEvents()
+ 
+        private IEnumerator LerpSkybox(Texture2D a, Texture2D b, float time)
         {
-            TimeSignals.Instance.onTimeStarted -= OnTimeStarted;
+            RenderSettings.skybox.SetTexture("_Texture1", a);
+            RenderSettings.skybox.SetTexture("_Texture2", b);
+            RenderSettings.skybox.SetFloat("_Blend", 0);
+            for (float i = 0; i < time; i += Time.deltaTime)
+            {
+                RenderSettings.skybox.SetFloat("_Blend", i / time);
+                yield return null;
+            }
+            RenderSettings.skybox.SetTexture("_Texture1", b);
         }
-
-        private void OnDisable()
+ 
+        private IEnumerator LerpLight(Gradient lightGradient, float time)
         {
-            UnSubscribeEvents();
+            for (float i = 0; i < time; i += Time.deltaTime)
+            {
+                globalLight.color = lightGradient.Evaluate(i / time);
+                RenderSettings.fogColor = globalLight.color;
+                yield return null;
+            }
         }
     }
 }
