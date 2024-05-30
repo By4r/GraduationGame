afe511f940d7933df43b0ecc5426e6a95905cbac
EmreOzhazer <emreozhazer@hotmail.com>
2024-05-30 18:42:28 +0300

    light switch added with sound

---
+++ Assets/Scripts/Runtime/Controllers/Item-Inventory/LightSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Runtime.SoundSystem;
+using UnityEngine;
+
+namespace Runtime.Controllers.Item_Inventory
+{
+    public class LightSwitch : MonoBehaviour
+    {
+        public GameObject controlledObject;
+        [SerializeField] private AudioManager _audioManager;
+        
+
+        public void ToggleObject()
+        {
+            _audioManager.PlayInteractSounds("LightSwitchSound");
+            controlledObject.SetActive(!controlledObject.activeSelf);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Runtime/Managers/LightSwitchManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Runtime.Controllers.Item_Inventory;
+using Runtime.Controllers.Player;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+namespace Runtime.Managers
+{
+    public class LightSwitchManager : MonoBehaviour
+    {
+        [SerializeField] private PlayerPhysicsController physicsController;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Ray raycast = physicsController.GetRaycast();
+                float range = physicsController.range;
+
+
+                if (Physics.Raycast(raycast, out RaycastHit hit, range))
+                {
+                    LightSwitch lightSwitch = hit.collider.GetComponent<LightSwitch>();
+
+                    if (hit.collider.CompareTag("LightSwitch"))
+                    {
+                        if (lightSwitch != null)
+                        {
+                            lightSwitch.ToggleObject();
+                            
+                        }else
+                        {
+                            Debug.LogWarning("Controlled object is not assigned in the LightSwitch component.");
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
