46c4bc3e2d1a8ac0ba6b16e24028977f391e9eac
By4r <j.alfarno142@gmail.com>
2024-05-30 21:52:17 +0300

    Task State Conflict Fixed

--- Assets/Scripts/Runtime/TaskStateSystem/TaskStateManager.cs
+++ Assets/Scripts/Runtime/TaskStateSystem/TaskStateManager.cs
@@ -51,7 +51,7 @@ namespace Runtime.TaskStateSystem
         private void Start()
         {
             // Initialize the state machine with an initial state
-            SetState(new PickUpPhoneState());
+            //SetState(new PickUpPhoneState());
             //SetState(new CollectGarbageState());
             //SetState(new GoSleepState());
             //SetState(new SweepFloorState());
